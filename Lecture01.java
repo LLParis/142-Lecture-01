c8f2de0a365ee3677776465297a4fcff1996a69b
Your Name <you@example.com>
2024-01-09 12:47:30 -0800

    Work In Progress

--- Lecture01.java
+++ Lecture01.java
@@ -1,8 +1,7 @@
-/**
- * Lecture01
- * REPLACE THIS LINE WITH YOUR NAME,
- * SO THE TEACHER KNOW WHO'S WORK THEY'RE GRADING :)
- */
+/*
+Lecture01
+London Paris
+*/
 public class Lecture01 {
 
   public static void main(String[] args) {
@@ -19,12 +18,30 @@ public class Lecture01 {
     // 'Toggle Line Comment' command)
 
     // Put your answer for #4 here:
+    // Collect Ingredients: 1 cup salted butter, 1 cup granulated sugar, 1 cup light brown sugar,
+    // 2 teaspoons pure vanilla extract
+    // 2 large eggs, 3 cups all-purpose flour, 1/2 teaspoon baking soda, 1 teaspoon sea salt,
+    // 2 cups chocolate chips
+
+    // 1. Preheat oven to 375 degrees F.
+    // 2. In a medium bowl, mix flour, baking soda, baking powder, and salt. Set aside.
+    // 3. Cream together butter and sugars until combined.
+    // 4. Beat in eggs and vanilla until light
+    // 5. Mix in the dry ingredients until combined.
+    // 6. Add chocolate chips and mix well
+    // 7. Roll 2-3 Tablespoons of dough at a time into balls and place them evenly spaced on your
+    // prepared cookie sheets.
+    // 8. Bake in preheated oven for approximately 10-12 minutes or until golden brown.
+    // 9. Let them sit on the baking pan for 2 minutes before moving to cooling rack.
 
     // Put your answer for #5 here:
+    // MyProgram.java is in human-readable code for the purpose of development.
+    // MyProgram.class has been converted from english to computer-readable byte code
 
     // Put your answer for #6 here:
     // please copy-and-paste the actual answers here (instead of listing A, D, R, Q,
     // etc)
+    // printIn, AnnualSalary, ABC, B4
 
     // Put your answer for #7 here:
     // please copy-and-paste the actual answers here (instead of listing A, D, R, Q,
