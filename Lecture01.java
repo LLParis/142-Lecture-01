777670f995e8b8861cfd963862494bcc154f1425
Your Name <you@example.com>
2024-01-09 11:32:37 -0800

    Initial Commit

---
+++ Lecture01.java
@@ -0,0 +1,43 @@
+/**
+ * Lecture01
+ * REPLACE THIS LINE WITH YOUR NAME,
+ * SO THE TEACHER KNOW WHO'S WORK THEY'RE GRADING :)
+ */
+public class Lecture01 {
+
+  public static void main(String[] args) {
+    System.out.println();
+    System.out.println("Hello, world!");
+    // Note: you will need to put some of your answers inside a comments
+    //
+    // Turn a line into a comment by putting // at the start of the line, like we're
+    // doing here
+    //
+    // You can comment a lot of lines at once in VSCode by
+    // highlighting the lines and then pressing Control/Command-/
+    // (or press Control/Command-P to bring up the Command Pallete and look for the
+    // 'Toggle Line Comment' command)
+
+    // Put your answer for #4 here:
+
+    // Put your answer for #5 here:
+
+    // Put your answer for #6 here:
+    // please copy-and-paste the actual answers here (instead of listing A, D, R, Q,
+    // etc)
+
+    // Put your answer for #7 here:
+    // please copy-and-paste the actual answers here (instead of listing A, D, R, Q,
+    // etc)
+
+    // Put your answer for #8 here:
+
+    // Put your answer for #16 here:
+    // NOTE: This code should compile and run!
+    System.out.println("My answer for question #16:");
+    // (VSCode/Prettier isn't letting me put a blank line after the code so I'll put
+    // this comment in, instead)
+
+    // Put your answer for #20 here:
+  }
+}
